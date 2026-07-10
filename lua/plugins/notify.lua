929a78068f63c6f0fe7ac0d416e60646095dd236
noldee <188809273+noldee@users.noreply.github.com>
2026-07-09 22:55:29 -0500

    fixing the visual aspect

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,11 +1,26 @@
 return {
 	"rcarriga/nvim-notify",
-
-	config = function()
+	event = "VeryLazy",
+	opts = {
+		background_colour = "#1c1c1c",
+		render = "compact",
+		stages = "static",
+		timeout = 1000,
+		top_down = true,
+		max_width = 60,
+		icons = {
+			ERROR = "",
+			WARN = "",
+			INFO = "",
+			DEBUG = "",
+			TRACE = "✎",
+		},
+	},
+	config = function(_, opts)
 		local notify = require("notify")
-
+		notify.setup(opts)
 		vim.notify = notify
 
-		notify("Welcome Back Sr 7w7")
+		notify("Welcome Back Sr 7w7", "info", { title = "Neovim" })
 	end,
 }
