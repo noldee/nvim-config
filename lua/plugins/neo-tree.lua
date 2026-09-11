83ffb46b861036a70fa42fc5c131e53329b13545
noldee <188809273+noldee@users.noreply.github.com>
2026-09-10 21:39:22 -0500

    feat(config): allow telescope and neo-tree to show hidden and env files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,4 @@
 return {
-
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	dependencies = {
@@ -7,4 +6,15 @@ return {
 		"MunifTanjim/nui.nvim",
 		"DaikyXendo/nvim-material-icon",
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Muestra los archivos filtrados de forma tenue
+					hide_dotfiles = false, -- Evita que se oculten los archivos que inician con punto (.)
+					hide_gitignored = false, -- Evita que se oculten los archivos del .gitignore (como .env)
+				},
+			},
+		})
+	end,
 }
