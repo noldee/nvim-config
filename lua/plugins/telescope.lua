cd0d8719a442fc00dc372250c9a9e8d59195bb37
noldee <188809273+noldee@users.noreply.github.com>
2026-09-10 21:34:07 -0500

    chore(telescope): show hidden and gitignored files like .env

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,17 @@ return {
 		require("telescope").setup({
 			pickers = {
 				colorscheme = { enable_preview = true },
+				-- Configuración para buscar archivos (incluye .env y ocultos)
+				find_files = {
+					hidden = true, -- Muestra archivos que empiezan con punto (.)
+					no_ignore = true, -- Muestra archivos listados en el .gitignore
+				},
+				-- Configuración para buscar texto dentro de archivos
+				live_grep = {
+					additional_args = function(_)
+						return { "--hidden", "--no-ignore" }
+					end,
+				},
 			},
 			defaults = {
 				layout_config = {
